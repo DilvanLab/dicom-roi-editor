cca0d953ac41e2c18aedd1bc1b8afef6adf89b1f
Dilvan Moreira <dilvan@gmail.com>
2016-12-15 10:18:44 -0200

    Test update copyright

--- resources/public/bower_components/dicom-roi-editor/ts/WebGLEditorImpl.ts
+++ resources/public/bower_components/dicom-roi-editor/ts/WebGLEditorImpl.ts
@@ -6,7 +6,7 @@
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License.
  *
- *     ePad is distributed in the hope that it will be useful,
+ *     ePad2 is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
